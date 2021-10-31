a81d1b2e0467bea2aec8681a823e5a371b9656f4
erfanmoshafi <erfan.moshafi2018@gmail.com>
2021-10-31 21:30:14 +0330

    add assets

---
+++ index.php
@@ -0,0 +1,12 @@
+<html>
+    <head>
+        <link rel="stylesheet" href="asset/css/bootstrp/bootstrap.min.css"/>
+        <link rel="stylesheet" href="asset/css/style.min.css"/>
+        <script src="assets/js/jquery.js"></script>
+        <script src="asset/js/bootstrap/bootstrap.min.js"></script>
+        <script src="asset/js/js.min.js"></script>
+    </head>
+    <body>
+
+    </body>
+</html>
