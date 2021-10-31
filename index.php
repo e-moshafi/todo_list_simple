34d6ce3b64eb5774df2046282d7578d674b6ce60
erfanmoshafi <erfan.moshafi2018@gmail.com>
2021-10-31 21:31:53 +0330

    base html

--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 <html>
     <head>
+        <title>TODO LIST </title>
+        <meta name="viewport" content= "width=device-width, initial-scale=1.0"> 
         <link rel="stylesheet" href="asset/css/bootstrp/bootstrap.min.css"/>
         <link rel="stylesheet" href="asset/css/style.min.css"/>
         <script src="assets/js/jquery.js"></script>
